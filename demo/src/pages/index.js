49632d61fd7654f3d449162de436976742198da4
Melanie Nolan <melanie.nolan@casumo.com>
2019-09-17 16:04:05 +0200

    initial commit

---
+++ demo/src/pages/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Grid} from 'gatsby-theme-instagram';
+
+const Index = () => {
+    return <Grid></Grid>;
+};
+
+export default Index;
---
+++ theme/src/hooks/useInstagramPosts.js
@@ -0,0 +1,30 @@
+import {useStaticQuery, graphql} from 'gatsby';
+
+const useInstagramPosts = () => {
+    const data = useStaticQuery(graphql`
+        {
+            allInstaNode {
+                nodes {
+                    id
+                    username
+                    caption
+                    localFile {
+                        childImageSharp {
+                            fluid {
+                                ...GatsbyImageSharpFluid_withWebp
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    `);
+    return data.allInstaNode.nodes.map(node => ({
+        ...node.localFile.childImageSharp.fluid,
+        id: node.id,
+        caption: node.caption,
+        username: node.username
+    }));
+};
+
+export default useInstagramPosts;
