56b3ec7935f35cc4af066eb68c26c2890b98e0ac
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-03-11 17:58:48 +0000

    add theme-ui

--- gatsby-theme-instagram/gatsby-config.js
+++ gatsby-theme-instagram/gatsby-config.js
@@ -1,12 +1,13 @@
-module.exports = ({username = 'zara'}) => ({
-    plugins: [
-        'gatsby-plugin-sharp',
-        'gatsby-transformer-sharp',
-        {
-            resolve: 'gatsby-source-instagram',
-            options: {
-                username: username
-            }
-        }
-    ]
+module.exports = ({ username = "zara" }) => ({
+  plugins: [
+    "gatsby-plugin-theme-ui",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-instagram",
+      options: {
+        username: username
+      }
+    }
+  ]
 });
