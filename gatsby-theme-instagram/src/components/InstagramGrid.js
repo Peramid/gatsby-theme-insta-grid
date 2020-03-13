29763086dbf9e934c4e6a97a987474521f2ae441
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-03-13 19:03:11 +0000

    use complex grid component

--- gatsby-theme-instagram/src/components/InstagramGrid.js
+++ gatsby-theme-instagram/src/components/InstagramGrid.js
@@ -1,70 +1,22 @@
 /** @jsx jsx  */
-import { jsx, Flex, Box, useColorMode, Styled } from "theme-ui";
-import { Fragment } from "react";
-import Image from "gatsby-image";
-import HeartIcon from "./HeartIcon";
+import { jsx, Flex } from "theme-ui";
 import useInstagramPosts from "../hooks/useInstagramPosts";
-
-function formatNumber(num) {
-  return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
-}
+import ComplexGrid from "./ComplexGrid";
 
 const InstagramGrid = () => {
   const instagramPosts = useInstagramPosts();
   console.log("posts", instagramPosts);
 
-  const [colorMode, setColorMode] = useColorMode();
-
   return (
-    <Fragment>
-      <Styled.h1>Username</Styled.h1>
-      <button
-        onClick={e => {
-          setColorMode(colorMode === "default" ? "dark" : "default");
-        }}
-      >
-        Dark mode
-      </button>
-      <Flex
-        sx={{
-          justifyContent: "center",
-          flexFlow: "row wrap",
-          alignItems: "stretch"
-        }}
-      >
-        {instagramPosts.map(post => (
-          <Box
-            key={post.id}
-            sx={{
-              flex: "0 1 300px",
-              m: 3,
-              backgroundColor: "background",
-              border: theme => `1px solid ${theme.colors.primary}`
-            }}
-          >
-            <Image
-              sx={{ width: "300px", height: "300px", objectFit: "cover" }}
-              fluid={post.image}
-            />
-
-            <div
-              sx={{
-                p: 2
-              }}
-            >
-              <p>
-                <span>
-                  <HeartIcon width={40} height={40} />
-                </span>
-                {`${formatNumber(post.likes)} likes`}
-              </p>
-
-              <p>{post.caption}</p>
-            </div>
-          </Box>
-        ))}
-      </Flex>
-    </Fragment>
+    <Flex
+      sx={{
+        justifyContent: "center",
+        flexFlow: "row wrap",
+        alignItems: "stretch"
+      }}
+    >
+      <ComplexGrid instagramPosts={instagramPosts} />
+    </Flex>
   );
 };
 
