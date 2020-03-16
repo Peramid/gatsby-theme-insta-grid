f05d8c7d84fd981b32825c90bf015601665e4b6d
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-03-16 12:20:26 +0000

    use card component

--- gatsby-theme-instagram/src/components/ComplexGrid.js
+++ gatsby-theme-instagram/src/components/ComplexGrid.js
@@ -1,40 +1,13 @@
 /** @jsx jsx  */
-import { jsx, Box } from "theme-ui";
+import { jsx } from "theme-ui";
 import { Fragment } from "react";
-import Image from "./Image";
-import HeartIcon from "./HeartIcon";
-import { formatNumber } from "../utils/utils";
+import Card from "./Card";
 
 const ComplexGrid = ({ instagramPosts }) => {
   return (
     <Fragment>
       {instagramPosts.map(post => (
-        <Box
-          key={post.id}
-          sx={{
-            flex: "0 1 300px",
-            m: 3,
-            backgroundColor: "background",
-            border: theme => `1px solid ${theme.colors.primary}`
-          }}
-        >
-          <Image fluid={post.image} alt={post.caption} />
-
-          <div
-            sx={{
-              p: 2
-            }}
-          >
-            <p>
-              <span>
-                <HeartIcon width={40} height={40} />
-              </span>
-              {`${formatNumber(post.likes)} likes`}
-            </p>
-
-            <p>{post.caption}</p>
-          </div>
-        </Box>
+        <Card post={post} />
       ))}
     </Fragment>
   );
