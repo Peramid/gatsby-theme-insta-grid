d8c7a103087652e78899c16e912ebef687443ee3
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-03-16 20:18:55 +0000

    try grid in a page

--- demo/src/pages/index.js
+++ demo/src/pages/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import { InstagramGrid } from "gatsby-theme-instagram";
 
 const Index = () => {
-  return <InstagramGrid gridType="complex"></InstagramGrid>;
+  return (
+    <div style={{ backgroundColor: "pink", paddingTop: "0.1em" }}>
+      <h1>Check out my Instagram!</h1>
+      <InstagramGrid></InstagramGrid>
+    </div>
+  );
 };
 
 export default Index;
