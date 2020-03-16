39120b6d3df9560afe89af0222e11e16f1238e9d
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-03-16 18:06:27 +0000

    change width of images

--- gatsby-theme-instagram/src/components/Image.js
+++ gatsby-theme-instagram/src/components/Image.js
@@ -5,7 +5,7 @@ import Img from "gatsby-image";
 const Image = ({ fluid, alt }) => {
   return (
     <Img
-      sx={{ width: "300px", height: "300px", objectFit: "cover" }}
+      sx={{ width: "100%", height: "300px", objectFit: "cover" }}
       fluid={fluid}
       alt={alt}
     />
