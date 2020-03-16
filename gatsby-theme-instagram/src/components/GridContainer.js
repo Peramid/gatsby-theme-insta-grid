ba904f7a7fa61e7f16362cc92efb447807b21d21
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-03-16 20:20:49 +0000

    rename to grid container

---
+++ gatsby-theme-instagram/src/components/GridContainer.js
@@ -0,0 +1,29 @@
+/** @jsx jsx  */
+import { jsx, Flex } from "theme-ui";
+import useInstagramPosts from "../hooks/useInstagramPosts";
+import ComplexGrid from "./ComplexGrid";
+import BasicGrid from "./BasicGrid";
+
+const GridContainer = ({ gridType = null }) => {
+  const instagramPosts = useInstagramPosts();
+  console.log("posts", instagramPosts);
+
+  return (
+    <Flex
+      sx={{
+        justifyContent: "center",
+        flexFlow: "row wrap",
+        alignItems: "stretch",
+        p: 3
+      }}
+    >
+      {gridType === "complex" ? (
+        <ComplexGrid instagramPosts={instagramPosts} />
+      ) : (
+        <BasicGrid instagramPosts={instagramPosts} />
+      )}
+    </Flex>
+  );
+};
+
+export default GridContainer;
