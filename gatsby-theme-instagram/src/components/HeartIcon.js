dfc2ba16dbdb8d4816719c31068f02a00f2e5517
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-03-17 11:27:11 +0000

    add heart icon component

---
+++ gatsby-theme-instagram/src/components/HeartIcon.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const HeartIcon = ({ width, height }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={width}
+    height={height}
+    viewBox="0 0 326 302"
+    fill="none"
+  >
+    <rect
+      id="Rectangle 1"
+      x="32"
+      y="173"
+      width="193.747"
+      height="182.322"
+      transform="rotate(-45 32 173)"
+      fill="#E3367B"
+    />
+    <ellipse
+      id="Ellipse 1"
+      cx="93.5"
+      cy="99"
+      rx="93.5"
+      ry="99"
+      fill="#E3367B"
+    />
+    <ellipse
+      id="Ellipse 2"
+      cx="238.5"
+      cy="95"
+      rx="87.5"
+      ry="95"
+      fill="#E3367B"
+    />
+  </svg>
+);
+
+export default HeartIcon;
