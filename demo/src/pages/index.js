3645e04ad3a24811256375315789737c2546c0fb
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-03-18 11:55:31 +0000

    import renamed component

--- demo/src/pages/index.js
+++ demo/src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { InstagramGrid } from "gatsby-theme-insta-grid";
+import { InstaGrid } from "gatsby-theme-insta-grid";
 
 const Index = () => {
   return (
     <div style={{ backgroundColor: "pink", paddingTop: "0.1em" }}>
       <h1>Check out my Instagram!</h1>
-      <InstagramGrid></InstagramGrid>
+      <InstaGrid></InstaGrid>
     </div>
   );
 };
