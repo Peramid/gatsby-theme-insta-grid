d46dbec73d8d8020e0e7cad821ce29091967d146
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-03-19 17:11:01 +0000

    create basic card component

---
+++ gatsby-theme-insta-grid/src/components/BasicCard.js
@@ -0,0 +1,21 @@
+/** @jsx jsx  */
+import { jsx, Card, Text } from "theme-ui";
+import Image from "./Image";
+
+const BasicCard = ({ post }) => {
+  return (
+    <Card
+      key={post.id}
+      variant="basic"
+      sx={{
+        flex: "0 1 300px"
+      }}
+    >
+      <a href={`https://www.instagram.com/p/${post.id}/`}>
+        <Image fluid={post.image} alt={post.caption} />
+      </a>
+    </Card>
+  );
+};
+
+export default BasicCard;
