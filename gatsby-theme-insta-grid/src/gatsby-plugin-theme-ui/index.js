e01a651dbc90ab32b338860435b510a3141ae1cf
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-03-24 22:14:24 +0000

    remove console.log

--- gatsby-theme-insta-grid/src/gatsby-plugin-theme-ui/index.js
+++ gatsby-theme-insta-grid/src/gatsby-plugin-theme-ui/index.js
@@ -1,33 +1,5 @@
 import { roboto as theme } from "@theme-ui/presets";
 
-console.log("theme", {
-  ...theme,
-  colors: {
-    ...theme.colors,
-    modes: {
-      dark: {
-        text: "#ffffff",
-        background: "grey"
-      }
-    }
-  },
-  cards: {
-    primary: {
-      padding: 2,
-      margin: 3,
-      borderRadius: 4,
-      boxShadow: "0 0 8px rgba(0, 0, 0, 0.125)",
-      background: "background"
-    },
-    compact: {
-      padding: 1,
-      borderRadius: 2,
-      border: "1px solid",
-      borderColor: "muted"
-    }
-  }
-});
-
 export default {
   ...theme,
   colors: {
