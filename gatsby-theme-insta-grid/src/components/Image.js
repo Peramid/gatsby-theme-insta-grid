84b692c8b459f9696c162f5bdcddd288e1b94b01
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-03-26 15:36:19 +0000

    remove image sizing

--- gatsby-theme-insta-grid/src/components/Image.js
+++ gatsby-theme-insta-grid/src/components/Image.js
@@ -3,13 +3,7 @@ import { jsx } from "theme-ui";
 import Img from "gatsby-image";
 
 const Image = ({ fluid, alt }) => {
-  return (
-    <Img
-      sx={{ width: "100%", height: "300px", objectFit: "cover" }}
-      fluid={fluid}
-      alt={alt}
-    />
-  );
+  return <Img fluid={fluid} alt={alt} />;
 };
 
 export default Image;
