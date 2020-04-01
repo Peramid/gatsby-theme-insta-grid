87deec7f445f94cb1e135974a093ca3bda587188
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-04-01 15:41:58 +0100

    refactor util function

--- gatsby-theme-insta-grid/src/utils/utils.js
+++ gatsby-theme-insta-grid/src/utils/utils.js
@@ -1,5 +1,5 @@
 export function formatNumber(num) {
-  if (num >= 1e3 && num < 1e6) return +(num / 1e3).toFixed(1) + "K";
-  if (num >= 1e6) return +(num / 1e6).toFixed(1) + "M";
+  if (num >= 1e3 && num < 1e6) return `${+(num / 1e3).toFixed(1)}k`;
+  if (num >= 1e6) return `${+(num / 1e6).toFixed(1)}m`;
   return num;
 }
