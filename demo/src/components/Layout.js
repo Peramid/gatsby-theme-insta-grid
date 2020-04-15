d8f5d23513c7d762865b55f1f4eecdd3e4f3c5ad
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-04-15 16:03:59 +0100

    use helmet with meta data

--- demo/src/components/Layout.js
+++ demo/src/components/Layout.js
@@ -2,16 +2,26 @@
 import { jsx } from "theme-ui";
 import { Fragment } from "react";
 import { Global } from "@emotion/core";
+import { Helmet } from "react-helmet";
 import Header from "./Header";
+import useSiteMetadata from "../hooks/useSiteMetadata";
 
 const Layout = ({ children }) => {
+  const { title, description } = useSiteMetadata();
+
   return (
     <Fragment>
+      <Helmet>
+        <html lang="en" />
+        <meta charSet="utf-8" />
+        <meta name="description" content={description} />
+        <title>{title}</title>
+      </Helmet>
       <Global
         styles={theme => ({
-          "*": {
-            boxSizing: "border-box"
-          }
+          // "*": {
+          //   boxSizing: "border-box"
+          // }
         })}
       />
       <Header />
