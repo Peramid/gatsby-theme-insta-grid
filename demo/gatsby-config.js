872d65c0bb4a6c43381d2d215d5908e8b8d27203
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-04-20 18:07:32 +0100

    update theme dependency to published version

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: "gatsby-theme-insta-grid",
+      resolve: "@melanienolan/gatsby-theme-insta-grid",
       options: {
         username: "nasa",
       },
