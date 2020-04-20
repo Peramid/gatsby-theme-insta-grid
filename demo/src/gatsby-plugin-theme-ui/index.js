fd499e5b2b97610f20ab835675a15a73d5e9d481
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-04-20 12:00:03 +0100

    add font and colors to theme

--- demo/src/gatsby-plugin-theme-ui/index.js
+++ demo/src/gatsby-plugin-theme-ui/index.js
@@ -2,44 +2,52 @@ import baseTheme from "gatsby-theme-insta-grid/src/gatsby-plugin-theme-ui";
 
 export default {
   ...baseTheme,
+  fonts: {
+    ...baseTheme.fonts,
+    body: "system-ui, sans-serif",
+    heading: '"Avenir Next", sans-serif',
+    monospace: "Menlo, monospace",
+  },
   styles: {
     ...baseTheme.styles,
-    root: { fontFamily: "body", lineHeight: "body", fontWeight: "body" },
     h1: {
       color: "text",
       fontFamily: "heading",
       lineHeight: "heading",
       fontWeight: "heading",
-      fontSize: 5
+      fontSize: 5,
     },
     h2: {
       color: "text",
       fontFamily: "heading",
       lineHeight: "heading",
       fontWeight: "heading",
-      fontSize: 4
+      fontSize: 4,
     },
     h3: {
       color: "text",
       fontFamily: "heading",
       lineHeight: "heading",
       fontWeight: "heading",
-      fontSize: 3
+      fontSize: 3,
     },
     p: {
       color: "text",
       fontFamily: "body",
       fontWeight: "body",
-      lineHeight: "body"
-    }
+      lineHeight: "body",
+    },
   },
   colors: {
-    ...baseTheme.colors
+    ...baseTheme.colors,
+    text: "#222222",
+    background: "#ffffff",
+    primary: "#3D5A80",
   },
   text: {
     ...baseTheme.text,
     numbers: {
-      ...baseTheme.text.numbers
-    }
-  }
+      ...baseTheme.text.numbers,
+    },
+  },
 };
