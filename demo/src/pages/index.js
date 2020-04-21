3321735caa1adcd59418d763e616a8d1359cdbea
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-04-21 12:43:35 +0100

    use styled elements

--- demo/src/pages/index.js
+++ demo/src/pages/index.js
@@ -2,28 +2,32 @@ import React from "react";
 import { InstaGrid } from "@melanienolan/gatsby-theme-insta-grid";
 import { Styled } from "theme-ui";
 import Layout from "../components/Layout";
+import Spacer from "../components/Spacer";
 
 export default () => (
   <Layout>
     <Styled.h2>Oh hey!</Styled.h2>
-    <p>
+    <Styled.p>
       If you want to add a grid of Instagram images to your Gatsby site, you're
       in the right place.
-    </p>
-    <p>
+    </Styled.p>
+    <Styled.p>
       Gatsby Theme Insta Grid gives you a customisable grid component to easily
       add a collection of images from either an Instagram account or hashtag to
       your site.
-    </p>
-    <p>
+    </Styled.p>
+    <Styled.p>
       Check out the docs{" "}
       <Styled.a href="https://github.com/melanienolan/gatsby-theme-insta-grid/">
         here
       </Styled.a>
       .
-    </p>
+    </Styled.p>
+    <Spacer space={"1rem"} />
+    <Styled.h2>You can add a basic grid:</Styled.h2>
     <InstaGrid />
-    <Styled.h2>And this is the Complex Grid</Styled.h2>
+    <Spacer space={"1rem"} />
+    <Styled.h2>Or a complex grid:</Styled.h2>
     <InstaGrid cardType="complex" />
   </Layout>
 );
